04aa70becb6fa9238f2e78ffe03db52bea485e10
paposeco <13892562+paposeco@users.noreply.github.com>
2021-10-01 14:22:30 +0200

    working on adding a delete button

--- src/App.js
+++ src/App.js
@@ -1,37 +1,72 @@
 import React from "react";
 import Overview from "./components/Overview.js";
+import uniqid from "uniqid";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { newtask: "" };
+    this.state = {
+      task: {
+        text: "",
+        id: uniqid(),
+        //o tasknumber começa a 0 e so adiciono um no submit pq o set change do input corre de cada vez que se escreve uma letra, e estava sempre a adicionar +1
+        tasknumber: 0,
+      },
+      arrayoftasks: [],
+      deleteTask: this.deleteTask.bind(this),
+    };
     this.handlerOfChange = this.handlerOfChange.bind(this);
     this.handlerOfSubmit = this.handlerOfSubmit.bind(this);
   }
   handlerOfChange = function (event) {
-    this.setState({ newtask: event.target.value });
+    this.setState({
+      task: {
+        text: event.target.value,
+        id: this.state.task.id,
+        tasknumber: this.state.task.tasknumber,
+      },
+    });
   };
 
   handlerOfSubmit = function (event) {
     event.preventDefault();
-    // isto esta mal
-    <Overview taskdescription="oi" />;
-    //    <Overview taskdescription={this.state.newtask} />;
+    this.setState({
+      arrayoftasks: this.state.arrayoftasks.concat(this.state.task),
+      task: {
+        text: "",
+        id: uniqid(),
+        tasknumber: this.state.task.tasknumber + 1,
+      },
+    });
   };
 
-  //value={this.state.value} no input text according to react mas afinal nao é preciso
+  deleteTask = function (atask) {
+    this.setState({
+      arrayoftasks: this.state.arrayoftasks.splice(atask, 1),
+    });
+  };
 
   render() {
+    // nao percebo bem estas cosntantes antes do return
+    const { task, arrayoftasks } = this.state;
+
     return (
       <div>
         <h1>Create a list of tasks</h1>
         <form onSubmit={this.handlerOfSubmit}>
           <label>
             Task:
-            <input type="text" onChange={this.handlerOfChange} />
+            <input
+              type="text"
+              onChange={this.handlerOfChange}
+              value={
+                task.text
+              } /* ao colocar isto aqui, quando se corre a funciona de submit, faz se clear ao text da task e limpa o input*/
+            />
           </label>
           <input type="submit" value="Add task" />
         </form>
+        <Overview tasks={arrayoftasks} deleteTaskMaybe={deleteTask} />
       </div>
     );
   }
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,16 +1,29 @@
 import React from "react";
 
-class Overview extends React.Component {
-  render() {
-    console.log(this.props);
-    return (
-      <div>
-        <ul>
-          <li>{this.props.taskdescription}</li>
-        </ul>
-      </div>
-    );
-  }
-}
+const Overview = function (props) {
+  // nao percebo {}
+  const { tasks } = props;
+  //  const {deleteTaskMaybe} =
+
+  return (
+    <ul>
+      {tasks.map((task, index) => {
+        return (
+          <li key={task.id}>
+            Task {task.tasknumber}: {task.text};
+            <button
+              id={"delBtn" + task.tasknumber}
+              onClick={() => this.props.deleteTask(task.tasknumber)}
+            >
+              Delete
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 export default Overview;
+
+//so estou a receber o array aqui. para receber o metodo tb tenho d eo meter no outro lado mas nao sei bem como
